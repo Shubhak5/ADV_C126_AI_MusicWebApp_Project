ceb0721b31f8709c096d7a1010d166ee1dd1f3a7
Shubhak5 <83910260+Shubhak5@users.noreply.github.com>
2021-10-05 19:23:27 +0530

    ADV_128_AI_MusicWebAPP_Project
    Added JS code to have poseNet model

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 song1 = "";
 song2 = "";
 
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rigthWristY = 0;
+
 function preload() {
     song1 = loadSound("music.mp3");
     song2 = loadSound("music2.mp3");
@@ -12,9 +17,28 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('poses', gotResult);
 }
 
 function draw() {
     image(video, 0, 0, 500, 500);
     //song2.play();
+}
+
+function modelLoaded() {
+    console.log("PoseNet Model is Initialized !");
+}
+
+function gotResult(results) {
+    console.log(results);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("leftWristX: " + leftWristX + " leftWristY: " + leftWristY)
+
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    console.log("rightWristX: " + rightWristX + "rightWristY: " + rightWristY);
 }
